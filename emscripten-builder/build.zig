bb4af9d6da690b0804a4cf80dc427737a1bbf403
milogreg <oldesoot@gmail.com>
2025-08-02 13:48:25 -0700

    Fix emscripten-builder

--- emscripten-builder/build.zig
+++ emscripten-builder/build.zig
@@ -1,39 +1,28 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-fn createEmsdkStep(b: *std.Build, emsdk: *std.Build.Dependency) *std.Build.Step.Run {
-    if (builtin.os.tag == .windows) {
-        return b.addSystemCommand(&.{emsdk.path("emsdk.bat").getPath(b)});
-    } else {
-        return b.addSystemCommand(&.{emsdk.path("emsdk").getPath(b)});
-    }
-}
-
-fn emSdkSetupStep(b: *std.Build, emsdk: *std.Build.Dependency) !?*std.Build.Step.Run {
-    const dot_emsc_path = emsdk.path(".emscripten").getPath(b);
-    const dot_emsc_exists = !std.meta.isError(std.fs.accessAbsolute(dot_emsc_path, .{}));
-
-    if (!dot_emsc_exists) {
-        const emsdk_install = createEmsdkStep(b, emsdk);
-        emsdk_install.addArgs(&.{ "install", "latest" });
-        const emsdk_activate = createEmsdkStep(b, emsdk);
-        emsdk_activate.addArgs(&.{ "activate", "latest" });
-        emsdk_activate.step.dependOn(&emsdk_install.step);
-        return emsdk_activate;
-    } else {
-        return null;
-    }
-}
+const emsdk_script_file = switch (builtin.os.tag) {
+    .windows => "emsdk.bat",
+    else => "emsdk",
+};
 
 pub fn build(b: *std.Build) !void {
     const emsdk = b.dependency("emsdk", .{});
-    if (try emSdkSetupStep(b, emsdk)) |emSdkStep| {
-        b.default_step.dependOn(&emSdkStep.step);
-    }
 
-    b.installDirectory(.{
+    const emsdk_install_dir = b.addInstallDirectory(.{
         .install_dir = .prefix,
         .install_subdir = "",
         .source_dir = emsdk.path(""),
     });
+
+    const emsdk_script_path = b.getInstallPath(.prefix, emsdk_script_file);
+
+    const emsdk_run_install = b.addSystemCommand(&.{ emsdk_script_path, "install", "latest" });
+    emsdk_run_install.step.dependOn(&emsdk_install_dir.step);
+
+    const emsdk_run_activate = b.addSystemCommand(&.{ emsdk_script_path, "activate", "latest" });
+    emsdk_run_activate.step.dependOn(&emsdk_install_dir.step);
+    emsdk_run_activate.step.dependOn(&emsdk_run_install.step);
+
+    b.default_step.dependOn(&emsdk_run_activate.step);
 }
